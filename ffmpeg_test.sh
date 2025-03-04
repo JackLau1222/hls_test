c278ed9cbdebfcce22f54feb9266cb74edd0615d
Jack Lau <jacklau1222@qq.com>
2025-03-04 10:41:15 +0800

    add seperate a and v stream files

--- ffmpeg_test.sh
+++ ffmpeg_test.sh
@@ -1,12 +1,19 @@
 ffmpeg -i big_bunny_1min_30fps.mp4 \
-  -map 0:v -c:v libx265 -preset medium -tag:v hvc1  \
-  -map 0:a -c:a aac -b:a 128k \
+  -c:v libx265 -tag hvc1\
+  -an \
   -f hls \
   -hls_time 10 \
   -hls_playlist_type vod \
   -hls_segment_type fmp4 \
   -hls_fmp4_init_filename init_hvc1.mp4 \
-  -hls_segment_filename "stream_%v/segment_%03d.m4s" \
-  -var_stream_map "v:0,a:0" \
-  -master_pl_name master.m3u8 \
-  "stream_%v/playlist.m3u8"
+  -hls_segment_filename "0/index_hvc1_%03d.m4s" \
+  0/index_hvc1.m3u8
+
+ffmpeg -i big_bunny_1min_30fps.mp4 \
+  -vn \
+  -c:a aac -b:a 128k \
+  -f hls \
+  -hls_time 10 \
+  -hls_playlist_type vod \
+  -hls_segment_filename "1/index_%03d.m4s" \
+  1/index.m3u8
